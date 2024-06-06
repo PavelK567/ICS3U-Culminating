3ef2fa3242ffda340daae5ac75ce8a388a4f5987
Pavel Kas. <pavel.kasyanenko@student.tdsb.on.ca>
2024-06-06 15:11:53 -0400

    Sending Commands

--- src/adventureGame.java
+++ src/adventureGame.java
@@ -9,38 +9,38 @@ import java.awt.event.ActionListener;
 
 public class adventureGame {
 
-    JTextArea room;
-    JTextArea log;
-    JTextArea items;
-    JTextField input;
-    JButton send;
+    // Initialize all the variables used in the game
+    String[] commandList = {"help","use"};
 
     
     public void play(){
         
+        // Start Initialize UI
         JFrame gameWindow = new JFrame("Adventure Game");
 
-        JTextArea room = new JTextArea("Area :");
+        // Initialize UI Elements
+        // Output Elements
         JTextArea log = new JTextArea(10, 20);
-        JTextArea items = new JTextArea("Items :");
+
+        JTextArea roomPanel = new JTextArea("Area :");
+        JTextArea itemsPanel = new JTextArea("Items :");
         JScrollPane logPane = new JScrollPane();
 
+
+        // Input Elements
         JTextField input = new JTextField(1);
         JButton send = new JButton("Send");
-        send.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){  
-                log.append(input.getText()+"\n"); 
-                input.setText(""); 
-            }  
-        });
 
+        // Set layout of game window
         gameWindow.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
+
         gbc.fill = GridBagConstraints.BOTH;
         gbc.anchor = GridBagConstraints.FIRST_LINE_START;
 
-        room.setEditable(false);
-        room.setColumns(20);
+        // Area display Set up 
+        roomPanel.setEditable(false);
+        roomPanel.setColumns(20);
         
         gbc.gridx = 0;
         gbc.gridy = 0;
@@ -49,8 +49,9 @@ public class adventureGame {
         gbc.gridheight = 1;
 
         
-        gameWindow.add(room,gbc);
+        gameWindow.add(roomPanel,gbc);
 
+        // Log Set up 
         log.setRows(5);
         log.setColumns(20);
         log.setEditable(false);
@@ -63,8 +64,9 @@ public class adventureGame {
 
         gbc.gridwidth = 3;
         gbc.gridheight = 3;
-        gameWindow.add(logPane,gbc);
+        gameWindow.add(log,gbc);
 
+        // Text entry area Set up 
         gbc.gridx = 0;
         gbc.gridy = 4;
 
@@ -72,30 +74,87 @@ public class adventureGame {
         gbc.gridheight = 1;
         gameWindow.add(input,gbc);
 
-        items.setRows(3);
-        items.setColumns(5);
-        items.setEditable(false);
+        // Items display Set up 
+        itemsPanel.setRows(3);
+        itemsPanel.setColumns(5);
+        itemsPanel.setEditable(false);
 
         gbc.gridx = 4;
         gbc.gridy = 0;
 
         gbc.gridwidth = 1;
         gbc.gridheight = 4;
-        gameWindow.add(items,gbc);
+        gameWindow.add(itemsPanel,gbc);
 
+        
+        // Send button Set up
         gbc.gridx = 4;
         gbc.gridy = 4;
 
         gbc.gridwidth = 1;
         gbc.gridheight = 1;
+
+        // Set Action for button
+        send.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){  
+                log.setText(toLog(log.getText(),runCommand(input.getText()))); 
+                input.setText(""); 
+            }  
+        });
+
         gameWindow.add(send,gbc);
 
+        // Finish setting up window
         gameWindow.setSize(300,250);
 
         gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         gameWindow.setVisible(true);
 
+    }
+
+    // Method to recieve and process a command
+    public String runCommand(String input){
+
+        // Get the first word of the input (the command word, i.e. "Use")
+        String cmd = input.split(" ")[0];
+        
+        // Second word of the input (the action item, i.e. "apple")
+        String item1 = null;
+        // OPTIONAL : Third word of the input (the acted item, i.e. "fridge"))
+        String item2 = null;
+
+        String output = "> " + input + "\n";
+        boolean invalid = true;
+        
+        // Check if command is invalid
+        for (int i = 0; i < commandList.length; i ++){
+            if (commandList[i].equals(cmd)){
+                invalid = false;
+            }
+        }
+
+        // Print "Command "x" not found" if command is invalid
+        if (invalid){
+            output += "Command \"" + cmd + "\" not found";
+        }
+
+        // "help" command
+        if (cmd.equals("help")){
+            if (item1 == null){
+                String commands = ""; 
+                for (int i = 0; i < commandList.length; i ++){
+                    commands = commands + commandList[i] + ", ";
+                }
+                output += ("Possible Commands : " + commands);
+            }
+        }
+        return output;
+    }
+
+    // Method for controlling text in the log
+    public String toLog(String logText, String text){
+        return (logText + "\n" + text);
     }
 
 
